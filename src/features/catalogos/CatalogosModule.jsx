7bdc1070e8a41e134a8654c66d9e2b3b0c6b3b64
Andri-Almengor <120224177+Andri-Almengor@users.noreply.github.com>
2026-07-10 14:59:40 -0600

    Encapsular módulo de catálogos

---
+++ src/features/catalogos/CatalogosModule.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import AppShell from '../../app/layout/AppShell';
+import CatalogosApp from '../../Catalogos';
+export default function CatalogosModule(){return <AppShell title="Catálogos"><div className="legacy-module catalogos-module"><CatalogosApp/></div></AppShell>}
