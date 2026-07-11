5683be5f70321923d129817763786eae26c44942
Andri-Almengor <120224177+Andri-Almengor@users.noreply.github.com>
2026-07-10 19:29:00 -0600

    feat: proteger rutas de boletas y agregar panel de catálogos

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,7 +5,7 @@ import ChangePasswordPage from '../pages/ChangePasswordPage';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import MorePage from '../pages/MorePage';
-import CategoriesPage from '../pages/admin/CategoriesPage';
+import CatalogsPage from '../pages/admin/CatalogsPage';
 import ClientsPage from '../pages/admin/ClientsPage';
 import TicketDetailPage from '../pages/tickets/TicketDetailPage';
 import TicketFormPage from '../pages/tickets/TicketFormPage';
@@ -17,26 +17,25 @@ import PermissionRoute from '../routes/PermissionRoute';
 import ProtectedRoute from '../routes/ProtectedRoute';
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<ProtectedRoute><AppShell /></ProtectedRoute>}>
-        <Route index element={<HomePage />} />
-        <Route path="boletas/pendientes" element={<TicketListPage status="PENDIENTE" />} />
-        <Route path="boletas/finalizadas" element={<TicketListPage status="FINALIZADA" />} />
-        <Route path="boletas/nueva" element={<TicketFormPage mode="create" />} />
-        <Route path="boletas/:boletaId" element={<TicketDetailPage />} />
-        <Route path="boletas/:boletaId/editar" element={<TicketFormPage mode="edit" />} />
-        <Route path="clientes" element={<ClientsPage />} />
-        <Route path="categorias" element={<CategoriesPage />} />
-        <Route path="cambiar-contrasena" element={<ChangePasswordPage />} />
-        <Route path="mas" element={<MorePage />} />
-        <Route path="usuarios" element={<PermissionRoute permission="USUARIOS_VER"><UsersPage /></PermissionRoute>} />
-        <Route path="usuarios/nuevo" element={<PermissionRoute permission="USUARIOS_GESTIONAR"><UserFormPage mode="create" /></PermissionRoute>} />
-        <Route path="usuarios/:usuarioId" element={<PermissionRoute permission="USUARIOS_VER"><UserDetailPage /></PermissionRoute>} />
-        <Route path="usuarios/:usuarioId/editar" element={<PermissionRoute permission="USUARIOS_GESTIONAR"><UserFormPage mode="edit" /></PermissionRoute>} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
-  );
+  return <Routes>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/" element={<ProtectedRoute><AppShell /></ProtectedRoute>}>
+      <Route index element={<HomePage />} />
+      <Route path="boletas/pendientes" element={<PermissionRoute permission="BOLETAS_VER"><TicketListPage status="PENDIENTE" /></PermissionRoute>} />
+      <Route path="boletas/finalizadas" element={<PermissionRoute permission="BOLETAS_VER"><TicketListPage status="FINALIZADA" /></PermissionRoute>} />
+      <Route path="boletas/nueva" element={<PermissionRoute permission="BOLETAS_CREAR"><TicketFormPage mode="create" /></PermissionRoute>} />
+      <Route path="boletas/:boletaUid" element={<PermissionRoute permission="BOLETAS_VER"><TicketDetailPage /></PermissionRoute>} />
+      <Route path="boletas/:boletaUid/editar" element={<PermissionRoute permission="BOLETAS_EDITAR"><TicketFormPage mode="edit" /></PermissionRoute>} />
+      <Route path="clientes" element={<PermissionRoute permission="CLIENTES_VER"><ClientsPage /></PermissionRoute>} />
+      <Route path="catalogos" element={<CatalogsPage />} />
+      <Route path="categorias" element={<Navigate to="/catalogos" replace />} />
+      <Route path="cambiar-contrasena" element={<ChangePasswordPage />} />
+      <Route path="mas" element={<MorePage />} />
+      <Route path="usuarios" element={<PermissionRoute permission="USUARIOS_VER"><UsersPage /></PermissionRoute>} />
+      <Route path="usuarios/nuevo" element={<PermissionRoute permission="USUARIOS_GESTIONAR"><UserFormPage mode="create" /></PermissionRoute>} />
+      <Route path="usuarios/:usuarioId" element={<PermissionRoute permission="USUARIOS_VER"><UserDetailPage /></PermissionRoute>} />
+      <Route path="usuarios/:usuarioId/editar" element={<PermissionRoute permission="USUARIOS_GESTIONAR"><UserFormPage mode="edit" /></PermissionRoute>} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Route>
+  </Routes>;
 }
