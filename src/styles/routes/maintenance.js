32f39677bfc6e4f91417af653a4de976284b3ab8
Andri-Almengor <120224177+Andri-Almengor@users.noreply.github.com>
2026-07-29 15:10:03 -0600

    Cargar estilos de firma pública en mantenimientos

--- src/styles/routes/maintenance.js
+++ src/styles/routes/maintenance.js
@@ -16,3 +16,4 @@ import '../maintenance-questions.css';
 import '../maintenance-question-cards.css';
 import '../maintenance-mobile-groups-collapse.css';
 import '../maintenance-location-groups-collapsible.css';
+import '../public-signature.css';
